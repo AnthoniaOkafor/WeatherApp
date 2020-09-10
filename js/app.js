d1595637722d14b5907676c1cca598fea923dfd2
AnthoniaOkafor <tonchuks@gmail.com>
2020-09-10 15:01:52 +0100

    Openweather api working fine

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const firstForm = document.querySelector(".first-form");
 const apiKey = 'a33ec5059bb874db8076287c5a7a5819';
-
+const apisection = document.querySelector(".api-section");
 
 
 
@@ -18,33 +18,32 @@ firstForm.addEventListener("submit", e => {
     fetch(url)
     .then(response => response.json())
     .then(data => {
-        return data;
+        const {main, name, sys, weather} = data;
+        const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png}`;
+        
+        
+        const markup = `
+            <h2 class="city-name" data-name="${name},${sys.country}">
+                <span>${name}</span>
+                <sup>${sys.country}</sup>
+            </h2>
+            <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup>
+            </div>
+            <figure>
+                <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
+                <figcaption>${weather[0]["description"]}</figcaption>
+            </figure>`;
+        
+
+
+        apisection.innerHTML = markup;
+
+        //msg.textContent = "";
+        //form.reset();
+        //input.focus();
     })
+
     .catch(() => {
-       // msg.innerHTML = "Please search for a valid city 😩";
-       console.log('not working');
+        msg.innerHTML = "Please search for a valid city 😩";
     });
-
-    const {main, name, sys, weather} = data;
-    const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png}`;
-    const apisection = document.querySelector(".api-section");
-    const card = document.createElement("div");
-    card.classList.add("city");
-    const markup = `
-        <h2 class="city-name" data-name="${name},${sys.country}">
-            <span>${name}</span>
-            <sup>${sys.country}</sup>
-        </h2>
-        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup>
-        </div>
-        <figure>
-            <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
-            <figcaption>${weather[0]["description"]}</figcaption>
-        </figure>`;
-    city.innerHTML = markup;
-    apisection.appendChild(card);
-
-    msg.textContent = "";
-    form.reset();
-    input.focus();
 });
